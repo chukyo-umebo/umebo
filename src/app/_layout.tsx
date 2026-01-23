b5e9cea2b699b2c0226c51107a9f4f87bd7e66ee
dokimiki <lmznd6yiew@gmail.com>
2026-01-24 04:43:51 +0900

    必要そうなパッケージを導入

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,12 +1,24 @@
-import { useLayoutEffect } from "react";
+import { useEffect, useLayoutEffect } from "react";
 import { useColorScheme } from "react-native";
-import { Stack } from "expo-router";
+import { Stack, useGlobalSearchParams, usePathname } from "expo-router";
 import { setStatusBarStyle } from "expo-status-bar";
 import * as SystemUI from "expo-system-ui";
+import analytics from "@react-native-firebase/analytics";
 
 export default function RootLayout() {
-    // ダークモード対応
+    const pathname = usePathname();
+    const params = useGlobalSearchParams();
     const colorMode = useColorScheme();
+
+    // Firebase Analytics で画面遷移を記録
+    useEffect(() => {
+        analytics().logScreenView({
+            screen_name: pathname,
+            screen_class: pathname,
+            params: JSON.stringify(params),
+        });
+    }, [pathname, params]);
+    // ダークモード対応
     useLayoutEffect(() => {
         SystemUI.setBackgroundColorAsync(colorMode === "dark" ? "#000" : "#ffffff");
         setStatusBarStyle(colorMode === "dark" ? "light" : "dark");
