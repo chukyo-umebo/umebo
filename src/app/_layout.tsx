a5d1d3e5b032f5cf7fa9a532d1803a64dc52bffb
dokimiki <lmznd6yiew@gmail.com>
2026-01-23 15:31:01 +0900

    ダークモード対応

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,25 @@
+import { useLayoutEffect } from "react";
+import { useColorScheme } from "react-native";
 import { Stack } from "expo-router";
+import { setStatusBarStyle } from "expo-status-bar";
+import * as SystemUI from "expo-system-ui";
 
 export default function RootLayout() {
-    return <Stack />;
+    // ダークモード対応
+    const colorMode = useColorScheme();
+    useLayoutEffect(() => {
+        SystemUI.setBackgroundColorAsync(colorMode === "dark" ? "#000" : "#ffffff");
+        setStatusBarStyle(colorMode === "dark" ? "light" : "dark");
+    }, [colorMode]);
+
+    return (
+        <Stack
+            screenOptions={{
+                contentStyle: { backgroundColor: "#0000" },
+                headerShown: false,
+            }}
+        >
+            <Stack.Screen name="index" />
+        </Stack>
+    );
 }
